d4875a3b5dc5f816c9af21674ea63b95ccd6b762
chengkoon <chengkoon@gmail.com>
2022-05-21 16:01:15 +0800

    removes react logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello, World!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
